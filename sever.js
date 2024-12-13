421c9708ad2baa372e3cfe1821c32180baf075d0
guava2247749 <i27153153@gmail.com>
2024-12-13 10:31:32 +0800

    12/13

--- sever.js
+++ sever.js
@@ -11,3 +11,20 @@ var DB = require("nedb-promises");
 var ProfolioDB = DB.create(__dirname+"/profolio.db");
 
 
+// ProfolioDB.insert([
+//     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+//     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+//     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+//     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+//     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+//     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+// ])
+
+server.post("/contact_me", (req,res)=>{
+    ContactDB.insert(req.body);
+    res.send("OK");
+})
+
+server.listen(80, ()=>{
+    console.log("Server is running at port 80.");
+})

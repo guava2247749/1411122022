fcbf0591898cf9887fdba93be578dec3d64df41d
guava2247749 <i27153153@gmail.com>
2025-01-03 07:04:08 +0800

    1/3

--- portfolio/js/portfolioVue.js
+++ portfolio/js/portfolioVue.js
@@ -4,8 +4,8 @@ createApp({
     data() {
         return{
             portfoliojs:[
-                {imageUrl:'images/portfolioCF/illustration_title.jpg',Palt:"aillustration_title",Plink:"Illustration.html",Pheading:"繪圖",Ptext:"日常畫的小廢圖"},
-                {imageUrl:'images/portfolioCF/GIF_title.gif',Palt:"animate_title",Plink:"gif.html",Pheading:"小動畫",Ptext:"自製GIF循環動畫"},
+                {imageUrl:'images/portfolioCF/illustration_0001.jpg',Palt:"aillustration_title",Plink:"Illustration.html",Pheading:"繪圖",Ptext:"日常畫的小廢圖"},
+                {imageUrl:'images/portfolioCF/GIF_0001.gif',Palt:"animate_title",Plink:"gif.html",Pheading:"小動畫",Ptext:"自製GIF循環動畫"},
                 {imageUrl:'images/portfolioCF/animate_title.png',Palt:"illustration_title",Plink:"animate.html",Pheading:"動畫",Ptext:"長篇動畫"}
             ]
         }
